2fbfdc39561646da88bdfd123324f7b9305203b7
deepshikhaa07 <143700551+deepshikhaa07@users.noreply.github.com>
2024-02-15 10:49:17 +0000

    Pending changes exported from your codespace

--- app.py
+++ app.py
@@ -1,7 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import sys
 
 app = Flask(__name__)
 
+todo_list_global=[]
+count=0
+
+class Todo:
+    def __init__(self, num, title):
+        self.id = num
+        self.title = title
+        self.complete = False
+    
+    def __str__(self):
+        return f"{self.title}"
+    
+    def updateComplete(self):
+        self.complete = True
+        return self.complete
+    
+    def updateNotComplete(self):
+        self.complete = False
+        return self.cpmplete
+    
+    def updateid(self, newid):
+        self.id = newid
 @app.route("/")
 def hello_world():
-    return render_template("index.html", title="Hello")
+    global todo_list_global
+    return render_template("index.html", todo_list = todo_list_global)
+
+@app.route("/add",methods=["POST"])
+def add():
+    global todo_list_global
+    global count
+    count = count + 1
+
+    title = request.form.get("title")
+
+    new_todo = Todo(num=count,title=title)
+    todo_list_global.append(new_todo)
+
+    return redirect("/")
